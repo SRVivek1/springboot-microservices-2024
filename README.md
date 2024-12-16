11358be0c5e4a6cd6e103eb3fce203e736322022
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-12-16 09:52:33 +0530

    return correct resp. status and link to newly created resource

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # springboot-microservices-2024
-microservices-with-springboot-and-spring-cloud
+
+## a2-sboot-ms-social-media-app
+- Return URL and create status in response.
+
+```
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+
+		logger.debug("User to save : {}", user);
+
+		User savedUser = userDaoService.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).body(savedUser);
+	}
+
+```
+
+- API in use.
+	- Below resource is used to build the URI dynamically.
+
+```
+org.springframework.web.servlet.support.ServletUriComponentsBuilder
+```
+---
+
+## a3-sboot-ms-social-
--- a2-sboot-ms-social-media-app/src/main/java/com/srvivek/sboot/mservices/controller/UserResource.java
+++ a2-sboot-ms-social-media-app/src/main/java/com/srvivek/sboot/mservices/controller/UserResource.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.srvivek.sboot.mservices.bean.User;
 import com.srvivek.sboot.mservices.dao.UserDaoService;
@@ -45,11 +47,22 @@ public class UserResource {
 		return userDaoService.findById(id);
 	}
 
+	/**
+	 * Create user.
+	 * 
+	 * @param user
+	 * @return
+	 */
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 
 		logger.debug("User to save : {}", user);
-		return userDaoService.save(user);
+
+		var savedUser = userDaoService.save(user);
+
+		var location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).body(savedUser);
 	}
 
 }
