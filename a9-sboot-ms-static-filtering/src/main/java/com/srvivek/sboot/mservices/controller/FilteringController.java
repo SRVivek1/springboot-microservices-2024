bc1c9bfafe4f04f939a6ad973362ee2872f14581
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-12-16 23:28:40 +0530

    minor changes

--- a9-sboot-ms-static-filtering/src/main/java/com/srvivek/sboot/mservices/controller/FilteringController.java
+++ a9-sboot-ms-static-filtering/src/main/java/com/srvivek/sboot/mservices/controller/FilteringController.java
@@ -1,5 +1,8 @@
 package com.srvivek.sboot.mservices.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,12 @@ public class FilteringController {
 	@GetMapping("/filtering")
 	public SomeBean filtering() {
 		return new SomeBean("Value-1", "Value-2", "Value-3", "Value-4", "Value-5", "Value-6");
+	}
+
+	@GetMapping("/filtering-list")
+	public List<SomeBean> filteringList() {
+		return Arrays.asList(new SomeBean("Value-1", "Value-2", "Value-3", "Value-4", "Value-5", "Value-6"),
+				new SomeBean("Value-11", "Value-22", "Value-33", "Value-44", "Value-55", "Value-66"),
+				new SomeBean("Value-111", "Value-222", "Value-333", "Value-444", "Value-555", "Value-666"));
 	}
 }
