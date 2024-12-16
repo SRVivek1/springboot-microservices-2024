40cf7f20b14f97cac6619173a1278f811dd4e448
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-12-16 13:27:41 +0530

    Request data validation

---
+++ a3-sboot-ms-validation/src/main/java/com/srvivek/sboot/mservices/A3RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.srvivek.sboot.mservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A3RestfulWebServicesApplication.class, args);
+	}
+
+}
