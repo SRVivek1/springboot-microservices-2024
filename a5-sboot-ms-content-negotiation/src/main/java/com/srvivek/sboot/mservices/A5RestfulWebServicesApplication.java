be580d4d8e82f59c3a8d93841613ee63ef04d7b4
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-12-16 13:54:54 +0530

    jackson-dataformat-xml for xml response

---
+++ a5-sboot-ms-content-negotiation/src/main/java/com/srvivek/sboot/mservices/A5RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.srvivek.sboot.mservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A5RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A5RestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ a5-sboot-ms-content-negotiation/src/test/java/com/srvivek/sboot/mservices/A5RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srvivek.sboot.mservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A5RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
