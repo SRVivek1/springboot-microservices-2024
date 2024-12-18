3f383598abdebe9eaed21260a86d5f0b8e5c211c
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-12-18 10:34:26 +0530

    customized sec. for httpbasic authentication

--- a14-sboot-sc-basic-authentication/src/main/java/com/srvivek/sboot/mservices/security/SpringSecurityConfiguration.java
+++ a14-sboot-sc-basic-authentication/src/main/java/com/srvivek/sboot/mservices/security/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.srvivek.sboot.mservices.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,6 +11,23 @@ public class SpringSecurityConfiguration {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+		// All requests must be authorized
+		// Else return HTTP 403
+		httpSecurity.authorizeHttpRequests(
+				authorizationManagerRequestMatcherRegistryCustomizer -> authorizationManagerRequestMatcherRegistryCustomizer
+						.anyRequest().authenticated());
+
+		// Prompt for authentication if request is not authorized
+		// Using default customizer
+		httpSecurity.httpBasic(Customizer.withDefaults());
+
+		/*
+		 * Disabling CSRF as it may cause issue with HTTP methods - POST & PUT.
+		 * 
+		 * if enabled, Keep prompting for user credentials for post request.
+		 */
+		httpSecurity.csrf(csrf -> csrf.disable());
 
 		return httpSecurity.build();
 	}
